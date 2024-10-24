56edce0a60e3b877929e31f334d4e1479206a792
sirayu2525 <taiyou1685@yahoo.ne.jp>
2024-10-24 10:54:48 +0900

    Update BoxController.cs

--- Assets/ShirasagiPuzzle/Code/Stage/BoxController.cs
+++ Assets/ShirasagiPuzzle/Code/Stage/BoxController.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxController : MonoBehaviour
+public class Box : MonoBehaviour
 {
     private Transform player;
     private bool isHeld = false;
     public float pickUpDistance = 1.5f; // 変更可能な持ち上げ距離
+    public Color boxColor;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        // レンダラーから色を初期化
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            boxColor = renderer.material.color;
+            Debug.Log("Box color initialized: " + boxColor);
+        }
+        else
+        {
+            Debug.Log("Renderer not found on Box object!");
+        }
+
     }
 
     void Update()
