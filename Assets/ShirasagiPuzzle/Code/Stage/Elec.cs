4aa0eba676e79554066d40c1b1846467a7488c98
ひいらぎ <1224syuto@gmail.com>
2024-10-27 17:45:35 +0900

    Update Elec.cs
    実体化するブロック(と鎖)を実装するに当たってElec.csを書き換えました。

--- Assets/ShirasagiPuzzle/Code/Stage/Elec.cs
+++ Assets/ShirasagiPuzzle/Code/Stage/Elec.cs
@@ -7,11 +7,16 @@ public class Elec : MonoBehaviour
 {
     [SerializeField] Sprite imageOff;
     [SerializeField] Sprite imageOn;
+    [SerializeField] BoxCollider boxCol;
     SpriteRenderer sr;
-
+    string obj;
+    bool touch;
     private void Start()
     {
         sr = gameObject.GetComponent<SpriteRenderer>();
+        obj = this.gameObject.name.Substring(0, 4);
+        if (obj == "Invi") boxCol.enabled = false;
+        //Debug.Log(obj);
     }
 
     // private void Update()
@@ -21,29 +26,48 @@ public class Elec : MonoBehaviour
     public void TurnOff()
     {
         if (sr != null) sr.sprite = imageOff;
+        if (touch == true)
+        {
+            touch = false;
+            Debug.Log(touch);
+        }
+        if (obj == "Invi") boxCol.enabled = false;
     }
     public void TurnOn()
     {
         if (sr != null) sr.sprite = imageOn;
+        if (touch == false)
+        {
+            touch = true;
+            Debug.Log(touch);
+        }
+        if (obj == "Invi") boxCol.enabled = true;
     }
     private void OnTriggerStay(Collider otherCollider)
     {
         var other = otherCollider.GetComponent<Collider>();
-        // if (other.tag == "Elec")
-        // {
-        //     Debug.Log("Elec");
-        // }
+
+        //if (other.tag == "Elec")
+        //{
+        //    Debug.Log("Elec");
+        //}
         switch (other.tag)
         {
-            case "Elec" or "Player":
-                // collidesWithPlayer = true;
-                int thisId = this.gameObject.GetInstanceID();
-                int otherId = other.gameObject.GetInstanceID();
-                ElecManager.instance.UfElecUniteByIds(thisId, otherId);
+            case "Elec":
+                int thisId1 = this.gameObject.GetInstanceID();
+                int otherId1 = other.gameObject.GetInstanceID();
+                ElecManager.instance.UfElecUniteByIds(thisId1, otherId1);
+                break;
+            case "Player":
+                //ここにboxのタグを追加するとboxでも電流を流せるようになります
+                //boxにPlayerのタグをつけると何故か動かなかったです
+                if (obj != "Invi" || touch == true)
+                {
+                    int thisId2 = this.gameObject.GetInstanceID();
+                    int otherId2 = other.gameObject.GetInstanceID();
+                    ElecManager.instance.UfElecUniteByIds(thisId2, otherId2);
+                }
                 break;
         }
-        // if (other.GetComponent<Collider>().tag == "Player")
-        // {
-        // }
     }
 }
